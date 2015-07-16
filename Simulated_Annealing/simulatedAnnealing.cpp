69c0d9d876061c19641b237f60babb9c62b2e9ce
Can Erdogan <canerdogan.89@gmail.com>
2015-07-16 08:21:15 -0400

    Got simulated annealing up to 24 queens with 1000 iterations. The decrease in number of wrong move jumps over time makes sense. Next: MDPs

--- Simulated_Annealing/simulatedAnnealing.cpp
+++ Simulated_Annealing/simulatedAnnealing.cpp
@@ -28,8 +28,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static const int numQueens = 4;
-int board [numQueens][numQueens];
+using namespace std;
+
+//static const int numQueens = 24;
+int** board;
+int numQueens;
 
 /* ********************************************************************************************* */
 /// Prints the board where 1 is filled by a queen and 0 is empty
@@ -43,7 +46,7 @@ void printBoard () {
 
 /* ********************************************************************************************* */
 /// Computes the number of collisions between queens
-int numCollisions () {
+int numCollisions (set <int>* attacked = NULL) {
 
 	// Check if the queens in two different rows are on the same column or diagonally in collision
 	int numColls = 0;
@@ -71,10 +74,18 @@ int numCollisions () {
 			}
 
 			// Check if they are on the same column
-			if(loc1 == loc2) numColls++;
+			if(loc1 == loc2) {
+				if(attacked) attacked->insert(i);
+				if(attacked) attacked->insert(j);
+				numColls++;
+			}
 			
 			// Check if they are diagonally in collision
-			if(((j - i) == (loc2 - loc1)) || ((j - i) == (loc1 - loc2))) numColls++;
+			if(((j - i) == (loc2 - loc1)) || ((j - i) == (loc1 - loc2))) {
+				if(attacked) attacked->insert(i);
+				if(attacked) attacked->insert(j);
+				numColls++;
+			}
 		}
 	}
 
@@ -86,7 +97,9 @@ int numCollisions () {
 void initializeBoard () {
 	
 	// Set all the cells to 0 for empty
-	memset(board, 0, sizeof(board));
+	for(size_t i = 0; i < numQueens; i++) 
+		for(size_t j = 0; j < numQueens; j++) 
+			board[i][j] = 0;
 
 	// For each row, choose a random column to place the queen
 	srand(time(NULL));
@@ -97,12 +110,96 @@ void initializeBoard () {
 }
 
 /* ********************************************************************************************* */
+int numChanceJumps [10];
+int MAX = 1000;
+int simulatedAnnealing () {
+
+	bool dbg = 0;
+	for(int n = 0; n < MAX; n++) {
+
+		// getchar();
+		if(dbg) printf("\n\n=== %d ====================================\n", n);
+		if(dbg) printBoard();
+
+		// Find the attacked queens
+		set <int> attacked;
+		int numColls = numCollisions(&attacked);
+		if(dbg) printf("numColls: %d, numAttacked: %d\n", numColls, attacked.size());
+		if(numColls == 0) {
+			printf("Success for %d queens! Num iters: %d\n", numQueens, n);
+			return n;
+		}
+
+		// Choose one of the attacked queens arbitrarily
+		int numAttacked = attacked.size();
+		int rand_idx = rand() % numAttacked;
+		set <int>::iterator it = attacked.begin();
+		advance(it, rand_idx);
+		int q_idx = *it;
+		
+		// Find the current column of the chosen queen
+		int prevCol = 0;
+		for(int i = 0; i < numQueens; i++) {
+			if(board[q_idx][i] == 1) {
+				prevCol = i;
+				break;
+			}
+		}
+	
+		// Check if moving the queen to an arbitrary column helps; if so make the move
+		int newCol = rand() % numQueens;
+		board[q_idx][prevCol] = 0;
+		board[q_idx][newCol] = 1;
+		int numColls2 = numCollisions();
+		if(numColls2 <= numColls) {
+			if(dbg) printf("Good move\n");
+			continue;
+		}
+
+		// If the move is not helpful, make it with some probability
+		double tn = 100.0 / (n + 1);
+		double P = exp((numColls-numColls2)/tn);
+		double random = ((double) rand()) / RAND_MAX;
+		if(random < P) {
+			numChanceJumps[n / 50] += 1;
+			if(dbg) printf("Lucky bad move: P: %lf, tn: %lf\n", P, tn);
+			if(dbg) 
+				printf("\tnumColls2: %d, numColls: %d, diff: %d\n", numColls2, numColls, numColls-numColls2);
+			continue;
+		}
+
+		// If chance doesn't help the bad move, undo it
+		board[q_idx][prevCol] = 1;
+		board[q_idx][newCol] = 0;
+		if(dbg) printf("No move\n");
+	}
+
+	printf("Fail.\n");
+	return MAX;
+}
+
+/* ********************************************************************************************* */
 int main (int argc, char* argv[]) {
+
+	// Get the number of queens
+	assert(argc > 1 && "Need # of queens");
+	numQueens = atoi(argv[1]);
+	board = new int* [numQueens];
+	for(int i = 0; i < numQueens; i++) 
+		board[i] = new int [numQueens];
 
 	// Initialize the board
 	initializeBoard();
+	memset(numChanceJumps , 0, sizeof(numChanceJumps));
 
+	// Do the annealing
+	int numIters = simulatedAnnealing();
+
+	// Print debugging data
+	printf("Number of chance jumps every %d iters:\n\t", MAX / 10);
+	for(int i = 0; i < numIters / 50; i++) printf("%d ", numChanceJumps[i]);
+	printf("\n\n\n");
 	printBoard();
-	printf("\n# of colls: %d\n", numCollisions());
+	
 }
 /* ********************************************************************************************* */
