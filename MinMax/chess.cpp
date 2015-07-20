ad793e913be578868ac265ca2daf3f0ebf44939c
Can Erdogan <canerdogan.89@gmail.com>
2015-07-20 18:03:08 -0400

    Played a game against a random computer. Next: Implement naive min-max with level limit

--- MinMax/chess.cpp
+++ MinMax/chess.cpp
@@ -48,11 +48,11 @@ void initBoard (bool default_ = true) {
 
 	// Debugging purposes
 	else {
-		positions[16] = 48;
-		positions[19] = 50;
-		positions[1] = 52;
-		positions[2] = 24;
-//		positions[17] = 61;
+		positions[P1] = 5*8 + 3;
+		//positions[K+16] = 4*8 + 4;
+		positions[P2+16] = 6*8 + 4;
+		//positions[P3] = 5*8 + 3;
+		//positions[C1+16] = 2*8 + 2;
 	}
 
 	// Fill in the board representation
@@ -97,7 +97,6 @@ void createMoves (vector <pair <int,int> >& moves, bool white) {
 		
 		// Create the position based on the piece
 		if(i < 9) {		// Pawn
-			continue;
 	
 			// Check if it can move forward if white
 			int forw_offset = white ? 1 : -1, new_vert = px + forw_offset;
@@ -118,66 +117,133 @@ void createMoves (vector <pair <int,int> >& moves, bool white) {
 			}
 		}
 
-		else if((i == 9) || (i == 16)) {	// Castle
-			
-			// Check if it can move board left from its position. Break if there is a collision.
-			for(int new_py = py - 1; new_py >= 0; new_py--) {
-				bool ownCollide = (!white && (board[px][new_py] > 16)) ||
-					(white && ((board[px][new_py] != 0) && (board[px][new_py] < 17)));
-				if(ownCollide) break;
-				else moves.push_back(make_pair(index, px*8+new_py));
-			}
-
-			// Check if it can move board right from its position. Break if there is a collision.
-			for(int new_py = py + 1; new_py < 8; new_py++) {
-				bool ownCollide = (!white && (board[px][new_py] > 16)) ||
-					(white && ((board[px][new_py] != 0) && (board[px][new_py] < 17)));
-				if(ownCollide) break;
-				else moves.push_back(make_pair(index, px*8+new_py));
-			}
-
-			// Check if it can move board up from its position. Break if there is a collision.
-			for(int new_px = px + 1; new_px < 8; new_px++) {
-				bool ownCollide = (!white && (board[new_px][py] > 16)) ||
-					(white && ((board[new_px][py] != 0) && (board[new_px][py] < 17)));
-				if(ownCollide) break;
-				else moves.push_back(make_pair(index, new_px*8+py));
-			}
-
-			// Check if it can move board bottom from its position. Break if there is a collision.
-			for(int new_px = px - 1; new_px >= 0; new_px--) {
-				bool ownCollide = (!white && (board[new_px][py] > 16)) ||
-					(white && ((board[new_px][py] != 0) && (board[new_px][py] < 17)));
-				if(ownCollide) break;
-				else moves.push_back(make_pair(index, new_px*8+py));
-			}
-		}
-
 		else if((i == 10) || (i == 15)) {	// Knight 
 
 			// Generate the 8 moves and check availability
-			
-		}
-		else if((i == 11) || (i == 14)) {	// Bishop 
-		}
-		else if(i == 12) {	// Queen 
-		}
-		else if(i == 13) {	// King 
+			static const int knight_xs [] = {1, 2, 2, 1, -1, -2, -2, -1};
+			static const int knight_ys [] = {-2, -1, 1, 2, 2, 1, -1, -2};
+			for(int move_idx = 0; move_idx < 8; move_idx++) {
+				
+				// Check if out of bounds
+				int new_px = px + knight_xs[move_idx], new_py = py + knight_ys[move_idx];
+				if((new_px < 0) || (new_px > 7) || (new_py < 0) || (new_py > 7)) continue;
+				
+				// Check if in collision with own side
+				bool ownCollide = (!white && (board[new_px][new_py] > 16)) ||
+					(white && ((board[new_px][new_py] != 0) && (board[new_px][new_py] < 17)));
+				if(ownCollide) continue;
+				
+				// Add to the moves
+				moves.push_back(make_pair(index, new_px*8+new_py));
+			}
 		}
 
-		// printf("%d: (%d, %d)\n", index, px, py);
+		else if(((i == 9) || (i == 16)) || ((i == 11) || (i == 14)) || (i == 12) || (i == 13))	{ 
+
+
+			// Check if it can move in the four directions
+			vector <int> xs, ys;
+			int MAX_MOVE = 8;
+			if((i == 9) || (i == 16)) {
+				int castle_xs [] = {1, 0, -1, 0}, castle_ys [] = {0, 1, 0, -1};
+				xs = vector <int> (castle_xs, castle_xs + 4), ys = vector <int> (castle_ys, castle_ys + 4);
+			}
+			else if((i == 11) || (i == 14)) {
+				int bishop_xs [] = {1, 1, -1, -1}, bishop_ys [] = {-1, 1, 1, -1};
+				xs = vector <int> (bishop_xs, bishop_xs + 4), ys = vector <int> (bishop_ys, bishop_ys + 4);
+			}
+			else if(i == 12) {
+				int queen_xs [] = {1, 1, -1, -1, 1, 0, -1, 0}, queen_ys [] = {-1, 1, 1, -1, 0, 1, 0, -1};
+				xs = vector <int> (queen_xs, queen_xs + 8), ys = vector <int> (queen_ys, queen_ys + 8);
+			}
+			else if(i == 13) {
+				int queen_xs [] = {1, 1, -1, -1, 1, 0, -1, 0}, queen_ys [] = {-1, 1, 1, -1, 0, 1, 0, -1};
+				xs = vector <int> (queen_xs, queen_xs + 8), ys = vector <int> (queen_ys, queen_ys + 8);
+				MAX_MOVE = 2;
+			}
+
+			for(int angle_idx = 0; angle_idx < xs.size(); angle_idx++) {
+				for(int move_idx = 1; move_idx < MAX_MOVE; move_idx++) {
+
+					// Get the new location
+					int new_px = px + move_idx * xs[angle_idx];
+					int new_py = py + move_idx * ys[angle_idx];
+
+					// Check if it is out of bounds
+					if((new_px < 0) || (new_px > 7) || (new_py < 0) || (new_py > 7)) break;
+
+					// Check collisions
+					bool ownCollide = (!white && (board[new_px][new_py] > 16)) ||
+						(white && ((board[new_px][new_py] != 0) && (board[new_px][new_py] < 17)));
+					bool theirCollide = (white && (board[new_px][new_py] > 16)) ||
+						(!white && ((board[new_px][new_py] != 0) && (board[new_px][new_py] < 17)));
+
+					// If collision with own, stop. If not, add and then stop.
+					if(ownCollide) break;
+					else {
+						moves.push_back(make_pair(index, new_px*8+new_py));
+						if(theirCollide) break;
+					}
+				}
+			}
+		}
+	}
+}
+
+/* ******************************************************************************************** */
+void makeMove (const pair<int,int>& move) {
+
+	// Make the changes to the position list for the mover (possibly attacker)
+	int currPos = positions[move.first];
+	board[currPos/8][currPos%8] = 0;
+	positions[move.first] = move.second;
+
+	// Check if there is a defender
+	if(board[move.second/8][move.second%8] != 0) {
+		positions.erase(board[move.second/8][move.second%8]);
 	}
 	
+	// Update the move on the board
+	board[move.second/8][move.second%8] = move.first;
+	printf("#positions: %lu\n", positions.size());
 }
 
 /* ******************************************************************************************** */
 int main (int argc, char* argv[]) {
 
-	initBoard(0);
+	srand(time(NULL));
+	initBoard();
 	printBoard();
 	vector <pair <int, int> > moves;
 	createMoves(moves, 1);
 	printf("#moves: %lu\n", moves.size());
 	for(int i = 0; i < moves.size(); i++) printf("(%d, %d)\n", moves[i].first, moves[i].second);
+	//makeMove(moves[1]);
+	printBoard();
+
+	while(true) {
+
+		// Get user input
+		char line[256];
+		char* p = gets (line);  /* Uh-oh. Don't panic. See below. */
+		int prevPos = (p[0] - 'a') + (p[1]-'1') * 8;
+		int nextPos = (p[2] - 'a') + (p[3]-'1') * 8;
+		int piece = board[(p[1]-'1')][p[0]-'a'];
+		printf("'%s': %d: %d -> %d\n", p, piece, prevPos, nextPos);
+
+		// Make the user move
+		pair <int, int> move = make_pair(piece, nextPos);
+		makeMove(move);
+		printBoard();
+		printf("===========================================\n");
+
+		// Let computer make a random move
+		vector <pair <int, int> > moves;
+		createMoves(moves, 0);
+		int random = rand() % moves.size();
+		makeMove(moves[random]);
+		printBoard();
+		printf("===========================================\n");
+	}
 }
 /* ******************************************************************************************** */
