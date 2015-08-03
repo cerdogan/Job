59ce0542bd4dd26e7cd0ef8b2489b3864751338c
Can Erdogan <canerdogan.89@gmail.com>
2015-08-03 16:30:24 -0400

    Almost done with debugging.

--- Partial_Order_Planning/pop.cpp
+++ Partial_Order_Planning/pop.cpp
@@ -30,10 +30,36 @@ struct Action {
 	vector <string> adds;  ///< The add effects as the action applied
 	vector <string> dels;  ///< The delete effects as the action applied
 	string args;
+	map <string, string> objs;
+
+	/// Prints action name
+	 string printName () const {
+
+		// Print the name 
+		char buf [256], temp [64];
+		sprintf(buf, "%s", name.c_str());
+
+		// Print the arguments
+		if(objs.size() > 0) {
+			sprintf(buf, "%s(", buf);
+			for(int i = 0; i < numObjects; i++) {
+				sprintf(temp, "arg%d", i+1);
+				map <string, string>::const_iterator it = objs.find(temp);
+				if(it != objs.end()) sprintf(buf, "%s%s,", buf, it->second.c_str());
+				else sprintf(buf, "%s%s,", buf, temp);
+			}
+			buf[strlen(buf)-1] = '\0';
+			sprintf(buf, "%s)", buf);
+		}
+
+		return string(buf);
+	}
 };
 
 vector <Action*> domainActions;
 
+/* ******************************************************************************************** */
+/// Causal relationship between two actions due to the precondition
 struct Causal {
 	const Action* pre;
 	const Action* post;
@@ -41,6 +67,7 @@ struct Causal {
 	Causal (const Action* f, const Action* s, string c) : pre(f), post(s), cause(c) {}
 };
 
+/* ******************************************************************************************** */
 /// Set of literals that define a state
 struct Plan {
 	set <const Action*> actions; //< may be uninstantiated	
@@ -102,14 +129,12 @@ void setup (Plan& p) {
 void getObjects (const string& lit, vector <string>& objs) {
 	objs.clear();
 	string middle = lit.substr(lit.find("("));
-  ps(middle);
 	int prev = -1;
 	for(int i = 0; i < middle.size(); i++) {
 		if((middle[i] == '(') || (middle[i] == ')') || (middle[i] == ',')) {
 			if(prev == -1) prev = i;
 			else {
 				objs.push_back(middle.substr(prev + 1, i - prev - 1));
-				ps(objs.back());
 				prev = i;
 			}
 		}
@@ -122,14 +147,24 @@ void getObjects (const string& lit, vector <string>& objs) {
 void orderingImplications (vector <pair <const Action*, const Action*> >& orderings, const 
 	Action* pre, const Action* post) {
 
+	printf("........................................................\n");
+	printf("pre: %s, post: %s\n", pre->printName().c_str(), post->printName().c_str());
 	vector <pair <const Action*, const Action*> > newOnes;
 	for(size_t i = 0; i < orderings.size(); i++) {
 
+		printf("ordering %lu: %s <- %s\n", i, orderings[i].first->printName().c_str(), orderings[i].second->printName().c_str());
+
 		// We have b < c: for a given a < b, add a < c
-		if(orderings[i].second == pre) newOnes.push_back(make_pair(orderings[i].first, post));
+		if(orderings[i].second == pre) {	
+			printf("\tpreCase: %s <- %s\n", orderings[i].first->printName().c_str(), post->printName().c_str());
+			newOnes.push_back(make_pair(orderings[i].first, post));
+		}
 		
 		// We have b < c: for a given c < d, add b < d
-		if(orderings[i].first == post) newOnes.push_back(make_pair(pre, orderings[i].second));
+		if(orderings[i].first == post) {
+			printf("\tpostCase: %s <- %s\n", pre->printName().c_str(), orderings[i].second->printName().c_str());
+			newOnes.push_back(make_pair(pre, orderings[i].second));
+		}
 	}
 
 	// Add the new ones to the current list
@@ -196,15 +231,20 @@ bool resolveThreats (const Plan& p) {
 				orderingImplications(p2.orderings, causal.post, *actIt);
 				return resolveThreats(p2);
 			}
-			else return false;
+			else {
+				printf("%s: returning false\n", __FUNCTION__);
+				return false;
+			}
 		}
 	}
 	
-	return false;
+	printf("%s: returning true\n", __FUNCTION__);
+	return true;
 }
 
 /* ******************************************************************************************** */
-bool tryAction (const Plan& p, const Action* act, pair<string,const Action*> prev) {
+bool tryAction (const Plan& p, const Action* act, pair<string,const Action*> prev, Plan& p2,
+		bool newAction = false) {
 
 	printf("vvvvvvvvvvvvvvvvvvv %s vvvvvvvvvvvvvv \n", __FUNCTION__);
 	printf("\t\tcalled with: %s: %s\n", act->name.c_str(), act->args.c_str());
@@ -220,7 +260,7 @@ bool tryAction (const Plan& p, const Action* act, pair<string,const Action*> pre
 	vector <string> objs;
 	for(int i = 0; i < act->adds.size(); i++) {
 
-		printf("\t"); ps(act->adds[i]);
+		printf("\tadd effect %d: ", i); ps(act->adds[i]);
 
 		// Check if the literal is the same type 
 		if(act->adds[i].substr(0, act->adds[i].find("(")).compare(subgoalName) == 0) {
@@ -229,10 +269,13 @@ bool tryAction (const Plan& p, const Action* act, pair<string,const Action*> pre
 			if(subgoalObjs.empty()) {
 
 				// Add a causal link between the previous action and this one
-				Plan p2 (p);
-				p2.causals.push_back(Causal(prevAct, act, subgoal));
-				p2.orderings.push_back(make_pair(prevAct, act));
-				orderingImplications(p2.orderings, prevAct, act);
+				p2 = Plan (p);
+				if(newAction) {
+					p2.actions.insert(act);
+				}
+				p2.causals.push_back(Causal(act, prevAct, subgoal));
+				p2.orderings.push_back(make_pair(act, prevAct));
+				orderingImplications(p2.orderings, act, prevAct);
 				printf("No objects: Adding action: %s: %s\n", act->name.c_str(), act->args.c_str());
 
 				// See if the threats can be resolved and a plan can be constructed after this choice
@@ -258,28 +301,92 @@ bool tryAction (const Plan& p, const Action* act, pair<string,const Action*> pre
 			}
 		
 			// If there are no conflicts, instantiate free variables of chosen action to the subgoal's needs
+			map <string, string> newObjs;
 			if(!conflicts) {
 				string newArgs = act->args;
 				ps(newArgs);
 				for(int j = 0; j < subgoalObjs.size(); j++) {
 					bool free2 = (objs[j].size() > 3) && (objs[j].substr(0,3).compare("arg") == 0);
-					if(free2) newArgs.replace(newArgs.find(objs[j]), objs[j].size(), subgoalObjs[j]);
+					if(free2) {
+						newObjs[objs[j]] = subgoalObjs[j];
+						int index = newArgs.find(objs[j]);
+						while(index != string::npos) {
+							newArgs.replace(index, objs[j].size(), subgoalObjs[j]);
+							index = newArgs.find(objs[j], index+1);
+						}
+					}
 				}
 
-				// Replace the chosen action with the instantiated action version
 				ps(newArgs);
+
+				// Replace the chosen action with the instantiated action version
 				Action* instAct = declareAction(act->name, act->numObjects, newArgs);
-				Plan p2 (p);
-				p2.actions.erase(act);
+				instAct->objs = newObjs;
+				p2 = Plan (p);
+				
+				// Add it to the list and update unachieved preconditions
 				p2.actions.insert(instAct);
+				if(newAction) {
+					for(int i = 0; i < instAct->pres.size(); i++) 
+						p2.unachieved.insert(make_pair(instAct->pres[i], instAct));
+				}
 
 				// Add causality and ordering
-				p2.causals.push_back(Causal(prevAct, instAct, subgoal));
-				p2.orderings.push_back(make_pair(prevAct, instAct));
-				orderingImplications(p2.orderings, prevAct, instAct);
+				p2.causals.push_back(Causal(instAct, prevAct, subgoal));
+				p2.orderings.push_back(make_pair(instAct, prevAct));
+				orderingImplications(p2.orderings, instAct, prevAct);
+
+				// If already exists in the plan in an uninstantiated form, update it
+				if(!newAction) {
+	
+					// Remove the data related to previous uninstantiated version
+					p2.actions.erase(act);
+					printf("\tRemoved uninstantiated action: %s: %s\n", act->name.c_str(), act->args.c_str());
+					map <string, const Action*>::iterator it = p2.unachieved.begin();
+					while(it != p2.unachieved.end()) {
+						if(it->second == act) {
+							string unachieved = it->first;
+							for(int j = 0; j < subgoalObjs.size(); j++) {			// replace the free argument if one exists
+								bool free2 = (objs[j].size() > 3) && (objs[j].substr(0,3).compare("arg") == 0);
+								if(free2) unachieved.replace(unachieved.find(objs[j]), objs[j].size(), subgoalObjs[j]);
+							}
+							std::map<string, const Action*>::iterator toErase = it;
+							++it;
+							p2.unachieved.erase(toErase);
+							p2.unachieved.insert(make_pair(unachieved, instAct));
+						}
+						else ++it;
+					}
+
+					// Transfer causality
+					for(size_t i = 0; i < p2.causals.size(); i++) {
+						if(p2.causals[i].pre == act) {		
+							p2.causals[i].pre = instAct;
+							for(int j = 0; j < subgoalObjs.size(); j++) {			// replace the free argument if one exists
+								bool free2 = (objs[j].size() > 3) && (objs[j].substr(0,3).compare("arg") == 0);
+								if(free2) p2.causals[i].cause.replace(p2.causals[i].cause.find(objs[j]), objs[j].size(), 
+									subgoalObjs[j]);
+							}
+						}
+						if(p2.causals[i].post == act) {
+							p2.causals[i].post = instAct;
+							for(int j = 0; j < subgoalObjs.size(); j++) {			// replace the free argument if one exists
+								bool free2 = (objs[j].size() > 3) && (objs[j].substr(0,3).compare("arg") == 0);
+								if(free2) p2.causals[i].cause.replace(p2.causals[i].cause.find(objs[j]), objs[j].size(), 
+									subgoalObjs[j]);
+							}
+						}
+					}
+
+					// Transfer causality and ordering data
+					for(size_t i = 0; i < p2.orderings.size(); i++) {
+						if(p2.orderings[i].first == act) p2.orderings[i].first = instAct;
+						if(p2.orderings[i].second == act) p2.orderings[i].second = instAct;
+					}
+				}
 				
 				// See if the threats can be resolved and a plan can be constructed after this choice
-				printf("With objects: Adding action: %s: %s\n", act->name.c_str(), act->args.c_str());
+				printf("With objects: Adding action: %s: %s\n", instAct->name.c_str(), instAct->args.c_str());
 				if(resolveThreats(p2)) return true;
 				else continue;
 			}
@@ -291,32 +398,71 @@ bool tryAction (const Plan& p, const Action* act, pair<string,const Action*> pre
 }
 
 /* ******************************************************************************************** */
-bool search (const Plan& p) {
+void printGraph (const Plan& p) {
+
+	FILE* graphFile = fopen("graph.dot", "w+");
+	fprintf(graphFile, "graph {\n");
+	for(int i = 0; i < p.orderings.size(); i++) {
+		const Action* act1 = p.orderings[i].first, *act2 = p.orderings[i].second;
+		fprintf(graphFile, "\"%s\" -- \"%s\"\n", act1->printName().c_str(), act2->printName().c_str());
+		printf("\"%s\" -- \"%s\"\n", act1->printName().c_str(), act2->printName().c_str());
+	}
+	fprintf(graphFile, "}\n");
+	fclose(graphFile);
+	system("dot -Tpng graph.dot -o graph.png");
+
+}
+
+/* ******************************************************************************************** */
+bool search (const Plan& p_) {
 
 	printf("vvvvvvvvvvvvvvvvvvv %s vvvvvvvvvvvvvv \n", __FUNCTION__);
 
-	// If all the actions are satisfied, return
-	if(p.unachieved.empty()) return true;
-	
-	// Choose a subgoal of any action in the plan that is not yet satisfied
-	size_t numUnachieved = p.unachieved.size();
-	map <string, const Action*>::const_iterator randIt = p.unachieved.begin();
-	advance(randIt, rand() % numUnachieved);
-	printf("Chosen subgoal: %s\n", randIt->first.c_str());
+	Plan p (p_), p2;
+	while(true) {
 
-	// Check if any of the actions already in the plan can satisfy this subgoal
-	for(set <const Action*>::const_iterator it = p.actions.begin(); it != p.actions.end(); it++) {
+		// Print state as a graph
+		printGraph(p);
 
-		// Look at the add conditions of the action
-		const Action* act = *it;
-		printf("Trying plan action.\n");
-		if(tryAction(p, act, *randIt)) return true;
-	}
+		// If all the actions are satisfied, return
+		if(p.unachieved.empty()) return true;
+		getchar();
+		
+		// Choose a subgoal of any action in the plan that is not yet satisfied
+		size_t numUnachieved = p.unachieved.size();
+		map <string, const Action*>::const_iterator randIt = p.unachieved.begin();
+		advance(randIt, rand() % numUnachieved);
+		printf("\n\n\n\n\n\nChosen subgoal: %s\n", randIt->first.c_str());
+		printf("\tprevAct: : %s: %s\n", randIt->second->name.c_str(), randIt->second->args.c_str());
 
-	// If no actions can be used from the current plan, choose a new action from the domain
-	for(int i = 0; i < domainActions.size(); i++) {
-		printf("Trying domain action.\n");
-		if(tryAction(p, domainActions[i], *randIt)) return true;
+		// Check if any of the actions already in the plan can satisfy this subgoal
+		bool addressedGoal = false;
+		for(set <const Action*>::const_iterator it = p.actions.begin(); it != p.actions.end(); it++) {
+
+			// Look at the add conditions of the action
+			const Action* act = *it;
+			printf("\nTrying old plan action.\n");
+			if(tryAction(p, act, *randIt, p2, false)) {
+				addressedGoal = true;
+				p2.unachieved.erase(randIt->first);
+				p = p2;
+				break;
+			}
+		}
+		if(addressedGoal) continue;
+
+		// If no actions can be used from the current plan, choose a new action from the domain
+		for(int i = 0; i < domainActions.size(); i++) {
+			printf("\nTrying new domain action.\n");
+			if(tryAction(p, domainActions[i], *randIt, p2, true)) {
+				p2.unachieved.erase(randIt->first);
+				addressedGoal = true;
+				p = p2;
+				break;
+			}
+		}
+		
+		if(!addressedGoal) return false;
 	}
 
 	return false;
