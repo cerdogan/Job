893cd96cd65dcb5721e26a8bb5f6507e64fdbc76
Can Erdogan <canerdogan.89@gmail.com>
2015-08-04 21:47:57 -0400

    Found a good piece of code for mouse control in linux so that I can control a 2D robot in continuous space. Next: Create GUI

---
+++ Monte_Carlo_Localization/mcl.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file mcl.cpp
+ * @author Can Erdogan
+ * @date 2015-08-04
+ * @brief Implementation of Monte-Carlo localization based on the explanation of Dellaert et al.'s
+ * ICRA '99 paper.
+ */
+
+#include <Eigen/Dense>
+#include <assert.h>
+#include <iostream>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <map>
+#include <sstream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+
+using namespace Eigen;
+using namespace std;
+
+typedef vector <Vector3d> State;
+
+/* ******************************************************************************************** */
+void prediction () {
+
+}
+
+/* ******************************************************************************************** */
+int main () {
+
+}
+/* ******************************************************************************************** */
